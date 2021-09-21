fe69239aa6815e30a42a8bb92f312cbcdf71e769
829295 <829295@mystma.org>
2021-09-21 14:50:57 -0500

    Title of Assignment try 1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #WRITE YOUR CODE HERE
-def wordSmash(a, b):
-    return a + b
+def topFiveMovies(a, b, c, d, e):
+    return a + " " + b + " " + c + " " + d + " " + e
 
+print(topFiveMovies("Shrek", "Shrek 2", "Shrek 3", "Shrek 4", "Shrek 5"))
